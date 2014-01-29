4f29fe7b9160b0192f3206b120499a84b9723954
claudyus <claudyus@HEX.(none)>
2014-01-29 22:59:24 +0100

    dev: circle basic implementation

--- dist/angular-google-maps.js
+++ dist/angular-google-maps.js
@@ -5574,4 +5574,50 @@ MarkerWithLabel.prototype.setMap = function (theMap) {
 
   // ... then deal with the label:
   this.label.setMap(theMap);
-};
+};;
+
+angular.module("google-maps").directive("circle", [
+  "$log", "$timeout", function ($log, $timeout) {
+
+    var DEFAULTS = {};
+    return {
+      restrict: "ECA",
+      require: "^googleMap",
+      replace: true,
+      scope: {
+        center: "=center",
+        radius: "=radius",
+        clickable: "=",
+        draggable: "=",
+        editable: "=",
+        geodesic: "=",
+        icons: "=icons",
+        visible: "=",
+        strokeColor: "=",
+        strokeOpacity: "=",
+        strokeWeight: "=",
+        fillColor: "=",
+        fillOpacity: "="
+      },
+      link: function(scope, element, attrs, mapCtrl) {
+        if (angular.isUndefined(scope.center) || scope.center === null) {
+          $log.error("circle: no valid center attribute found");
+          return;
+        }
+        return $timeout(function() {
+          var map = mapCtrl.getMap();
+          $log.info(scope.center);
+          $log.info(map);
+          opts = {center: new google.maps.LatLng(scope.center.latitude, scope.center.longitude),
+            radius: scope.radius,
+            strokeColor: scope.strokeColor || '#F00',
+            strokeOpacity: scope.strokeOpacity || 0.8,
+            fillColor: scope.fillColor || '#F00',
+            fillOpacity: scope.fillOpacity || 0.35,
+            map: map };
+          circle = new google.maps.Circle(opts);
+        });
+      }
+    };
+  }
+]);
